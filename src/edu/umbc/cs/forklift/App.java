ea4ebfc980412713d521de18dbfb70e905a5ad8b
Kautiontape <kautiontape@gmail.com>
2016-06-13 12:13:24 -0400

    Added pom.xml and Eclipse files

---
+++ src/edu/umbc/cs/forklift/App.java
@@ -0,0 +1,11 @@
+package edu.umbc.cs.forklift;
+
+import burlap.oomdp.core.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
