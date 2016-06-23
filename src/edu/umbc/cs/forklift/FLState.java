e537d51744c66240d74b4cf8c1bfd6abec43fa94
khalil8500 <khalil8500@gmail.com>
2016-06-23 15:51:22 -0400

    adding other variables for the interface

--- src/edu/umbc/cs/forklift/FLState.java
+++ src/edu/umbc/cs/forklift/FLState.java
@@ -10,27 +10,33 @@ import burlap.mdp.core.state.StateUtilities;
 import static edu.umbc.cs.forklift.forklift.ATT_X;
 import static edu.umbc.cs.forklift.forklift.ATT_Y;
 import static edu.umbc.cs.forklift.forklift.ATT_D;
+import static edu.umbc.cs.forklift.forklift.ATT_W;
+import static edu.umbc.cs.forklift.forklift.ATT_L;
 
 public class FLState implements MutableState{
 
 	private double direction;
 	private double x;
 	private double y;
+	private double yLength;
+	private double xWidth;
 	
 	private static final List<Object> keys = Arrays.<Object>asList(ATT_X, ATT_Y, ATT_D);
 	
 	public FLState(){
 	}
 	
-	public FLState(double x, double y, double direction)
+	public FLState(double x, double y, double direction, double yLength, double xWidth)
 	{
 		this.x = x;
 		this.y = y;
 		this.direction = direction;
+		this.yLength = yLength;
+		this.xWidth = xWidth;
 	}
 	
 	public State copy() {
-		return new FLState(x, y, direction);
+		return new FLState(x, y, direction, yLength, xWidth);
 	}
 
 	public Object get(Object variableKey) {
@@ -45,6 +51,14 @@ public class FLState implements MutableState{
 			{
 				return direction;
 			}
+			else if(variableKey.equals(ATT_W))
+			{
+				return xWidth;
+			}
+			else if(variableKey.equals(ATT_L))
+			{
+				return yLength;
+			}
 			else{
 				throw new RuntimeException("Unknown key " + variableKey);
 			}
@@ -58,6 +72,14 @@ public class FLState implements MutableState{
 			}
 			else if((Integer)variableKey == 2){
 				return direction;
+			}
+			else if((Integer)variableKey == 3)
+			{
+				return xWidth;
+			}
+			else if((Integer)variableKey == 4)
+			{
+				return yLength;
 			}
 			else{
 				throw new RuntimeException("Unknown key " + variableKey);
--- src/edu/umbc/cs/forklift/forklift.java
+++ src/edu/umbc/cs/forklift/forklift.java
@@ -1,5 +1,6 @@
 package edu.umbc.cs.forklift;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import burlap.mdp.auxiliary.DomainGenerator;
@@ -10,6 +11,7 @@ import burlap.mdp.core.TerminalFunction;
 import burlap.mdp.core.state.MutableState;
 import burlap.mdp.core.state.State;
 import burlap.mdp.singleagent.SADomain;
+import burlap.mdp.singleagent.common.GoalBasedRF;
 import burlap.mdp.singleagent.model.RewardFunction;
 import burlap.mdp.singleagent.model.statemodel.FullStateModel;
 
@@ -18,6 +20,8 @@ public class forklift implements DomainGenerator{
 	public static final String ATT_X = "x";
 	public static final String ATT_Y = "y";
 	public static final String ATT_D = "d";
+	public static final String ATT_L = "l";
+	public static final String ATT_W = "w";
 	
 	public static final String ACTION_FORWARD = "forward";
 	public static final String ACTION_BACKWARDS = "backward";
@@ -29,9 +33,20 @@ public class forklift implements DomainGenerator{
 	public static final int SOUTH = 2;
 	public static final int WEST = 3;
 	
+	public final double xBound = 20;
+	public final double yBound = 20;
+	
 	protected RewardFunction rf;
 	protected TerminalFunction tf;
+		
+	double velocity = 0.5;
+	double rotVel = 5;
 	
+	public List<Double> goalArea; //xmin,xmax,ymin,ymax
+	public ArrayList<FLState> Walls = new ArrayList<FLState>();
+	public ArrayList<FLState> Boxes = new ArrayList<FLState>();
+	
+	public static int captured = 0; 
 	
 	public TerminalFunction getTf() {
 		return tf;
@@ -53,7 +68,10 @@ public class forklift implements DomainGenerator{
 		
 		SADomain domain = new SADomain();
 		
+		FLModel fmodel = new FLModel(velocity, rotVel);
 		
+		tf = new FLTF(Boxes, goalArea);
+		rf = new GoalBasedRF(new FLRF(Boxes, goalArea), 1, 0);
 		
 		return null;
 	}
@@ -93,22 +111,94 @@ public class forklift implements DomainGenerator{
 				((MutableState)s).set(ATT_D, direction);
 			}
 			else if(actionName.equals(ACTION_FORWARD)){
-				double deltax = Math.cos(direction) * speed;
-				double deltay = Math.sin(direction) * speed;
+				double deltax = Math.cos((direction/360)*2*Math.PI) * speed;
+				double deltay = Math.sin((direction/360)*2*Math.PI) * speed;
 				px += deltax;
 				py += deltay;
+				((MutableState)s).set(ATT_X, px);
+				((MutableState)s).set(ATT_Y, py);
 			}
 			else if(actionName.equals(ACTION_BACKWARDS)){
-				double deltax = Math.cos(direction) * speed;
-				double deltay = Math.sin(direction) * speed;
+				double deltax = Math.cos((direction/360)*2*Math.PI) * speed;
+				double deltay = Math.sin((direction/360)*2*Math.PI) * speed;
 				px -= deltax;
 				py -= deltay;
+				((MutableState)s).set(ATT_X, px);
+				((MutableState)s).set(ATT_Y, py);
 			}
 			return s;
 		}
 
 		public List<StateTransitionProb> stateTransitions(State s, Action a) {
 			return FullStateModel.Helper.deterministicTransition(this, s, a);
+		}
+		
+	}
+	
+	public static class FLTF implements TerminalFunction
+	{
+		
+		private ArrayList<FLState> Boxes;
+		private List<Double> goal;
+		
+		public FLTF(ArrayList<FLState> Boxes, List<Double> goal)
+		{
+			this.Boxes = Boxes;
+			this.goal = goal;
+		}
+		public boolean isTerminal(State s) {
+			for(FLState b: Boxes)
+			{
+				if((Double)b.get(forklift.ATT_X) < goal.get(0))
+				{
+					return false;
+				}
+				else if((Double)b.get(forklift.ATT_X) > goal.get(1))
+				{
+					return false;
+				}
+				else if((Double)b.get(forklift.ATT_Y) < goal.get(2))
+				{
+					return false;
+				}
+				else if((Double)b.get(forklift.ATT_Y) > goal.get(3))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		
+	}
+	
+	public static class FLRF implements TerminalFunction
+	{
+		private ArrayList<FLState> Boxes;
+		private List<Double> goal;
+		
+		public FLRF(ArrayList<FLState> Boxes, List<Double> goal)
+		{
+			this.Boxes = Boxes;
+			this.goal = goal;
+		}
+		
+		public boolean isTerminal(State arg0) {
+			int captured = 0;
+			for(FLState b: Boxes)
+			{
+				if((Double)b.get(forklift.ATT_X) > goal.get(0) && 
+						(Double)b.get(forklift.ATT_X) < goal.get(1) &&
+						(Double)b.get(forklift.ATT_Y) > goal.get(2) &&
+						(Double)b.get(forklift.ATT_Y) < goal.get(3)){
+					captured++;
+				}
+			}
+			if(captured > forklift.captured)
+			{
+				forklift.captured = captured;
+				return true;
+			}
+			return false;
 		}
 		
 	}
