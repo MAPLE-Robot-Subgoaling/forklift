ca93b4c37000ab64bda1f4d9940403f6451a1de3
khalil8500 <khalil8500@gmail.com>
2016-07-18 12:47:06 -0400

    added room generation method

--- src/edu/umbc/cs/forklift/ForkliftClass.java
+++ src/edu/umbc/cs/forklift/ForkliftClass.java
@@ -1,6 +1,8 @@
 package edu.umbc.cs.forklift;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import burlap.mdp.singleagent.SADomain;
 import burlap.mdp.singleagent.environment.SimulatedEnvironment;
@@ -21,7 +23,7 @@ public class ForkliftClass {
 		ArrayList<FLBlock> Walls = new ArrayList<FLBlock>();
 		for(int i = 0; i < 10; i++)
 		{
-			Walls.add(new FLBlock.FLWall(i, 10, 1, 1, "wall"+i));
+			Walls.add(new FLBlock.FLWall(i, 10, 1, 1, "Wall " + i + ", " + 10));
 		}
 		FLState state = new FLState(agent, Walls);
 		SimulatedEnvironment env = new SimulatedEnvironment(domain, state);
@@ -38,5 +40,48 @@ public class ForkliftClass {
 
 		exp.initGUI();
 
+	}
+	
+	//preconditions: x1 < x2 and y1 < y2
+	private static List<FLWall> GenerateRoom(int x1, int x2, int y1, int y2, Map<Integer, List<Double>> doors){
+		List<FLWall> room = new ArrayList<FLWall>();
+		for(int i = x1; i < x2; i++){
+			
+			boolean addY1 = true;
+			for(List<Double> door: doors.values()){
+				if(i == door.get(0) && y1 == door.get(1))
+					addY1 = false;
+			}
+			if(addY1){
+				room.add(new FLWall(i, y1, 1, 1, "Wall " + i + ", "+ y1));
+			}
+			boolean addY2 = true;
+			for(List<Double> door: doors.values()){
+				if(i == door.get(0) && y2 == door.get(1))
+					addY2 = false;
+			}
+			if(addY2){
+				room.add(new FLWall(i, y2, 1, 1, "Wall " + i + ", "+ y2));
+			}
+		}
+		for(int j  = y1+1; j < y2-1; j++){
+			boolean addX1 = true;
+			for(List<Double> door: doors.values()){
+				if(j == door.get(1) && x1 == door.get(0))
+					addX1 = false;
+			}
+			if(addX1){
+				room.add(new FLWall(x1, j, 1, 1, "Wall " + x1 + ", "+ j));
+			}
+			boolean addX2 = true;
+			for(List<Double> door: doors.values()){
+				if(j == door.get(1) && x2 == door.get(0))
+					addX2 = false;
+			}
+			if(addX2){
+				room.add(new FLWall(x2, j, 1, 1, "Wall " + x2 + ", "+ j));
+			}
+		}
+		return room;
 	}
 }
