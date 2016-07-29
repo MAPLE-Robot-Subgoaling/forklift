47c6d03b98ae9e0f07612612c20fde6521eb6ad1
sparr1 <sparr@umass.edu>
2016-07-29 12:30:17 -0400

    FLState to support boxes

--- src/edu/umbc/cs/forklift/state/FLState.java
+++ src/edu/umbc/cs/forklift/state/FLState.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.math3.util.OpenIntToDoubleHashMap.Iterator;
+
 import burlap.mdp.core.oo.state.MutableOOState;
 import burlap.mdp.core.oo.state.OOStateUtilities;
 import burlap.mdp.core.oo.state.ObjectInstance;
 import burlap.mdp.core.oo.state.exceptions.UnknownClassException;
 import burlap.mdp.core.state.MutableState;
+import edu.umbc.cs.forklift.state.FLBlock.FLBox;
 import edu.umbc.cs.forklift.state.FLBlock.FLWall;
 import edu.umbc.cs.forklift.state.FLBlock;
 
@@ -18,10 +21,13 @@ public class FLState implements MutableOOState{
 	
 	FLAgent agent;
 	List <FLBlock> walls;
+	List <FLBlock> boxes;
 
 	private static final List<Object> keys = Arrays.<Object>asList(CLASS_AGENT,CLASS_WALL,CLASS_BOX);
 	
 	public FLState(){
+		walls = new ArrayList<FLBlock>();
+		boxes = new ArrayList<FLBlock>();
 	}
 	
 	public FLState(FLAgent agent, List<FLBlock> walls)
@@ -29,9 +35,12 @@ public class FLState implements MutableOOState{
 		this.agent = agent;
 		this.walls = walls;
 	}
-	
+	public FLState(FLAgent agent, List<FLBlock> walls, List<FLBlock> boxes){
+		this(agent, walls);
+		this.boxes = boxes;
+	}
 	public FLState copy() {
-		return new FLState(agent, walls);
+		return new FLState(agent, walls, boxes);
 	}
 	
 	public List<Object> variableKeys() {
@@ -44,7 +53,7 @@ public class FLState implements MutableOOState{
 
 	//TODO when FLState is more object oriented, fill these out
 	public int numObjects() {
-		return 1 + walls.size();
+		return 1 + walls.size()+boxes.size();
 	}
 
 	public ObjectInstance object(String s) {
@@ -52,10 +61,11 @@ public class FLState implements MutableOOState{
 			return agent;
 		else{
 			for(FLBlock w: walls)
-			{
-				if(w.name() == s)
+				if(w.name().equals(s))
 					return w;
-			}
+			for(FLBlock b: boxes)
+				if(b.name().equals(s))
+					return b;
 		}
 		return null;
 	}
@@ -64,44 +74,49 @@ public class FLState implements MutableOOState{
 		List<ObjectInstance> obj = new ArrayList<ObjectInstance>();
 		obj.add(agent);
 		for(FLBlock w: walls)
-		{
 			obj.add(w);
-		}
+		for(FLBlock b: boxes)
+			obj.add(b);
 		return obj;
 	}
 
 	public List<ObjectInstance> objectsOfClass(String s) {
-		List<ObjectInstance> ooc = new ArrayList<ObjectInstance>();;
-		if(agent.className().equals(s)){
+		List<ObjectInstance> ooc = new ArrayList<ObjectInstance>();
+		if(s.equals(CLASS_AGENT))
 			ooc.add(agent);
-		}
-		else if(walls.get(0).className().equals(s))
-		{
+		else if(s.equals(CLASS_WALL))
 			for(FLBlock w: walls)
-			{
 				ooc.add(w);
-			}
-		}
+		else if(s.equals(CLASS_BOX))
+			for(FLBlock b: boxes)
+				ooc.add(b);
 		return ooc;
 	}
 
 	public MutableOOState addObject(ObjectInstance o) {
-		if(o instanceof FLAgent){
+		if(o instanceof FLAgent)
 			agent = (FLAgent)o;
-		}
-		else if(o instanceof FLWall){
+		else if(o instanceof FLWall)
 			walls.add((FLWall)o);
-		}
-		else{
+		else if(o instanceof FLBox)
+			boxes.add((FLBox)o);
+		else
 			throw new UnknownClassException(o.className());
-		}
 
 		return this;
 	}
 
 	public MutableOOState removeObject(String s) {
-		if(agent.name().equals(s)){
-			agent = new FLAgent(); //cannot remove, so copy
+		if(agent.name().equals(s))
+			agent = new FLAgent(); //cannot remove, so copy over with blank slate
+		else{
+			//use fancy iterator as list modification occurs
+			for(java.util.Iterator<FLBlock> iterator = walls.iterator(); iterator.hasNext();)
+				if(iterator.next().name().equals(s))
+					iterator.remove();
+			for(java.util.Iterator<FLBlock> iterator = boxes.iterator(); iterator.hasNext();)
+				if(iterator.next().name().equals(s))
+					iterator.remove();
 		}
 		return this;
 	}
@@ -119,6 +134,8 @@ public class FLState implements MutableOOState{
 			return agent;
 		else if(key.equals(CLASS_WALL))
 			return walls;
+		else if(key.equals(CLASS_BOX))
+			return boxes;
 		return null;
 	}
 
@@ -127,6 +144,8 @@ public class FLState implements MutableOOState{
 			agent = (FLAgent) value;
 		else if(key.equals(CLASS_WALL))
 			walls = (List<FLBlock>) value;
+		else if(key.equals(CLASS_BOX))
+			boxes = (List<FLBlock>) value;
 		return this;
 	}
 	
